067d3e59c9dfa517fb211cff0ff355ac8a3f921a
Shikaru0 <games1logins1tech@gmail.com>
2026-09-18 13:41:55 +0200

    Add MinecraftClientMixin to set itemUseCooldown to 0

---
+++ src/client/java/name/modid/client/mixin/MinecraftClientMixin.java
@@ -0,0 +1,19 @@
+package name.modid.client.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public abstract class MinecraftClientMixin {
+    @Shadow
+    private int itemUseCooldown;
+
+    @Inject(method = "handleInputEvents", at = @At("HEAD"))
+    private void resetItemUseCooldown(CallbackInfo ci) {
+        this.itemUseCooldown = 0;
+    }
+}
